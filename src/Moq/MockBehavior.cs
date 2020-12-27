fdc644f72894f63e395ea7f7fe13acb60ffca4c0
stakx <stakx@eml.cc>
2020-12-27 16:54:34 +0100

    Reorder `MockBehavior` enum values
    such that the default enum value will equal `default(MockBehavior)`.

--- src/Moq/MockBehavior.cs
+++ src/Moq/MockBehavior.cs
@@ -9,20 +9,22 @@ namespace Moq
 	public enum MockBehavior
 	{
 		/// <summary>
-		/// Causes the mock to always throw 
-		/// an exception for invocations that don't have a 
+		/// Default mock behavior, which equals <see cref="Loose"/>.
+		/// </summary>
+		Default = default,
+
+		/// <summary>
+		/// Will never throw exceptions, returning default
+		/// values when necessary (null for reference types,
+		/// zero for value types or empty enumerables and arrays).
+		/// </summary>
+		Loose = default,
+
+		/// <summary>
+		/// Causes the mock to always throw
+		/// an exception for invocations that don't have a
 		/// corresponding setup.
 		/// </summary>
 		Strict,
-		/// <summary>
-		/// Will never throw exceptions, returning default  
-		/// values when necessary (null for reference types, 
-		/// zero for value types or empty enumerables and arrays).
-		/// </summary>
-		Loose,
-		/// <summary>
-		/// Default mock behavior, which equals <see cref="Loose"/>.
-		/// </summary>
-		Default = Loose,
 	}
 }
